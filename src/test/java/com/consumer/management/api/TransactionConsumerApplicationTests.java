caa74776a20100fce335441ca36b4626f740b754
ericksdantas144 <ericksdantas144@gmail.com>
2024-10-14 14:18:23 -0300

    initial commit

---
+++ src/test/java/com/consumer/management/api/TransactionConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consumer.management.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
